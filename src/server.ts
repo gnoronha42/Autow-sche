64f13b10fe6ba18aaf78f28f49ab8a3c73feb2cc
Gabriel <=>
2020-10-15 15:10:08 -0300

    modificado

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, { json } from 'express';
 
+import router from './routes'
+
 const app = express();
 
 app.use(json());
+app.use(router);
 
 app.get('/', (req, res) => {
     res.json({ message: "Ola API" });
