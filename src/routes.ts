8b6b030fcfd040a0c280fa9d39d4637311569fc6
Gabriel <=>
2020-10-16 22:03:23 -0300

    Update Routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,15 @@
 import express from 'express'
 
+import PointsController from './controllers/PointsController';
+import WashController from './controllers/WashController';
+
+//instancia as classes do Controller
+const pointsController = new PointsController();
+const washController = new WashController();
+
 const router = express.Router();
+
+router.post('/points', pointsController.create)
+router.get('/wash', washController.index)
 
 export default router;
